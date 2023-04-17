49e08f8e4a6204adb6575536565060bdbc2173a4
Duong Long <longduongbao29@gmail.com>
2023-04-17 11:23:25 +0700

    add-student

--- src/public/scripts/add_student.js
+++ src/public/scripts/add_student.js
@@ -1,3 +1,8 @@
-$(document).on('click', '#add-student-submit', function () {
-    
-});
+function showForm() {
+    var form = document.getElementById("add-student-form");
+    form.style.display = "block";
+}
+function hideForm() {
+    var form = document.getElementById("add-student-form");
+    form.style.display = "none";
+}
